0b3a4a2c99d588ca0b8021c617f7f15bb13797d9
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-07-26 05:16:06 +0900

    CLI のメインエントリを修正
    ファイルの読み込み機能を追加実装

--- UiBeam.Finder.Cli/Program.fs
+++ UiBeam.Finder.Cli/Program.fs
@@ -1,6 +1,10 @@
 ﻿open System
+open System.IO
+open System.Diagnostics
+open System.Reflection
 open UiBeam.Finder.Core
 
+// その文字の座標が "ういビーム" の文字座標のどれかに引っかかるかを調べます
 let rec isUiBeam (uibeamList: (int*int) list list) ((l, c): int*int) (uibeamIndex: int) (charIndex: int) =
     if uibeamIndex < List.length uibeamList then
         let uibeam = uibeamList.[uibeamIndex]
@@ -19,38 +23,61 @@ let rec isUiBeam (uibeamList: (int*int) list list) ((l, c): int*int) (uibeamInde
     else
         false 
 
-let test = @"ういビームとは、
-イラストレーター兼 VTuber のしぐれうい氏が放つ必殺技である(というのは嘘で、ファンの間での身内ネタである)。ちなみに、
-微細構造定数とは、電磁相互作用の強さを示す定数であり、
-1を137で割った値に近い、
-無次元量である。"
-
-let arr = TextMatrix.ToCharMatrix test
-printfn "[探索する文字列]\r\n%s" test
-printfn ""
-
-let finder = new Finder(arr)
-let uibeamList = finder.Start()
-//printfn "uibeamList = %A" uibeamList
-
-let uibeamCount = uibeamList |> List.length
-if uibeamCount > 0 then
-    printfn "%d 件のういビームが発見されました!" uibeamCount
+// テキストからういビームを探索して結果を出力します
+let find (text: string) =
+    let arr = TextMatrix.ToCharMatrix text
+    printfn "[探索する文字列]\r\n%s" text
     printfn ""
-    let mutable lineIndex = 0
-    for line in arr do
-        let mutable charIndex = 0
-        for c in line do
-            let isUiBeamResult = isUiBeam uibeamList (lineIndex, charIndex) 0 0
-            if isUiBeamResult then
-                Console.ForegroundColor <- ConsoleColor.Red
-                Console.Write(c)
-                Console.ResetColor()
-            else
-                Console.Write(c)
-            charIndex <- charIndex + 1
+
+    let finder = new Finder(arr)
+    let uibeamList = finder.Start()
+    //printfn "uibeamList = %A" uibeamList
+
+    let uibeamCount = uibeamList |> List.length
+    if uibeamCount > 0 then
+        printfn "%d 件のういビームが発見されました!" uibeamCount
+        printfn ""
+        let mutable lineIndex = 0
+        for line in arr do
+            let mutable charIndex = 0
+            for c in line do
+                let isUiBeamResult = isUiBeam uibeamList (lineIndex, charIndex) 0 0
+                if isUiBeamResult then
+                    Console.ForegroundColor <- ConsoleColor.Red
+                    Console.Write(c)
+                    Console.ResetColor()
+                else
+                    Console.Write(c)
+                charIndex <- charIndex + 1
+            Console.Write("\r\n")
+            lineIndex <- lineIndex + 1
         Console.Write("\r\n")
-        lineIndex <- lineIndex + 1
-    Console.Write("\r\n")
-else
-    printfn "ういビーム検出器はういビームを発見できませんでした．"
+    else
+        printfn "ういビーム検出器はういビームを発見できませんでした．"
+
+// The Main Entry as follow:
+
+let assemblyFilePath = Assembly.GetExecutingAssembly().Location
+
+let appName = FileVersionInfo.GetVersionInfo(assemblyFilePath).ProductName
+let appVersion = FileVersionInfo.GetVersionInfo(assemblyFilePath).ProductVersion
+let developerName = FileVersionInfo.GetVersionInfo(assemblyFilePath).CompanyName
+
+printfn "---------------------------------------------------------------------------"
+printfn "%s ver.%s" appName appVersion
+printfn "by %s" developerName
+printfn "---------------------------------------------------------------------------"
+
+printfn "テキストファイルの文字列からういビームを発見します．"
+while true do
+    printfn "テキストファイルのパスを入力:"
+
+    let filePath = Console.ReadLine()
+    if File.Exists(filePath) then
+        try
+            File.ReadAllText(filePath) |> find
+        with
+        |_ as ex -> printfn "エラー\r\n%s\r\n" ex.Message
+    else
+        printfn "ファイル '%s' は存在しません．\r\n" filePath
+
